b35c2d9e14def212d82b94cc21642222c24d73ba
Vinicius Frias <vmofrias@gmail.com>
2024-08-25 12:16:09 -0300

    Mental Health Gateway

---
+++ src/main/java/br/com/mh/MentalHealthGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.mh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalHealthGatewayApplication.class, args);
+	}
+
+}
